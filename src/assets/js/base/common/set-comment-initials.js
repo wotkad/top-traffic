cc4f769c835cb8fcaedebd312eca9a8a0dd89da9
wotkad <wotkad@gmail.com>
2025-03-10 19:37:57 +0300

    update

---
+++ src/assets/js/base/common/set-comment-initials.js
@@ -0,0 +1,17 @@
+function setCommentInitials() {
+  $('.message__comment').each(function() {
+    const $title = $(this).find('.message__head h3');
+    let fullName = $title.text().trim();
+
+    fullName = fullName.replace(/[^a-zA-Zа-яА-ЯёЁ\s]/g, '');
+
+    const initials = fullName
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+
+      $(this).find('.message__avatar span').text(initials);
+  });
+  
+}
+setCommentInitials();
