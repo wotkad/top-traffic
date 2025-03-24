12facf9792b7e2ee66202995a651a816872846d7
wotkad <wotkad@gmail.com>
2025-03-24 13:15:30 +0300

    update

--- src/assets/js/components/toggle/toggle-popup.js
+++ src/assets/js/components/toggle/toggle-popup.js
@@ -30,12 +30,27 @@ function togglePopup() {
     $('.popup').removeClass('active');
     $('.popup__bg').removeClass('active');
   });
+
+
+  $(document).on('mouseup', function(e) {
+    $('.popup.active').each(function() {
+      let popup = $(this);
+      let popupWrapper = popup.find('.popup__wrapper');
+      if (!popupWrapper.is(e.target) && !popupWrapper.has(e.target).length) {
+        closePopup(popup, popup.prev());
+      }
+    });
+  });
 }
 
 function closePopup(popup, popupBg, currentMessage) {
   popup.removeClass('active');
   popupBg.removeClass('active');
-  currentMessage.removeClass('active');
+  
+  // Проверяем наличие currentMessage и удаляем класс, если он есть
+  if (currentMessage && currentMessage.length) {
+    currentMessage.removeClass('active');
+  }
 
   // Включаем прокрутку, только если все попапы закрыты
   if (!$('.popup.active').length) {
