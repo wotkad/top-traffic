431d79de048646169eb44333361c42ca2b791cb3
wotkad <wotkad@gmail.com>
2025-06-27 00:34:46 +0300

    update

--- src/assets/js/components/toggle/toggle-filter-sort.js
+++ src/assets/js/components/toggle/toggle-filter-sort.js
@@ -1,15 +1,29 @@
 function toggleFilterSort() {
   let button = $('.table th .table__sort');
+  
   button.on('mouseover', function() {
     $(this).addClass('sorted');
   });
+  
   button.on('mouseleave', function() {
-    $(this).removeClass('sorted');
+    if (!$(this).hasClass('active')) {
+      $(this).removeClass('sorted');
+    }
   });
+  
   button.on('click', function() {
-    button.not(this).find('.table__sortbutton').removeClass('active');
-    $(this).find('.table__sortbutton').toggleClass('active');
-    $(this).toggleClass('active');
+    // Убираем активные классы у других кнопок
+    button.not(this).removeClass('active reverse').find('.table__sortbutton').removeClass('active');
+    
+    // Для текущей кнопки
+    if (!$(this).hasClass('active')) {
+      // Первый клик - добавляем active
+      $(this).addClass('active').find('.table__sortbutton').addClass('active');
+    } else {
+      // Последующие клики - toggle reverse
+      $(this).toggleClass('reverse');
+    }
   });
 }
+
 toggleFilterSort();
