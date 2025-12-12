e0832765e12af4d3d36a1864244c67b4dd97e890
wotkad <wotkad@gmail.com>
2025-12-12 14:29:37 +0300

    update

--- src/assets/js/components/toggle/toggle-reasons.js
+++ src/assets/js/components/toggle/toggle-reasons.js
@@ -1,22 +1,25 @@
 function toggleReasons() {
-  $(document).on('input', '.popup[data-popup-name="add-approved-lists"] .dropdown-priority .dropdown__item input', function () {
-      const popup = $('.popup[data-popup-name="add-approved-lists"]');
-      const greenChecked = popup.find('.dropdown-priority .dropdown__item input[data-color="priority-green"]').is(':checked');
-      const redChecked   = popup.find('.dropdown-priority .dropdown__item input[data-color="priority-red"]').is(':checked');
+  $(document).on('input', '.popup[data-popup-name="add-approved-lists"], .popup[data-popup-name="edit-approved-lists"] .dropdown-priority .dropdown__item input', function () {
+
+      const popup = $(this).closest('.popup'); // нужный попап
+
+      const greenChecked = popup
+          .find('.dropdown-priority .dropdown__item input[data-color="priority-green"]')
+          .is(':checked');
+
+      const redChecked = popup
+          .find('.dropdown-priority .dropdown__item input[data-color="priority-red"]')
+          .is(':checked');
 
       const reasons = popup.find('.reasons__items');
 
       if (greenChecked) {
-          // Добавляем disabled
           reasons.addClass('disabled');
-
-          // Снимаем ВСЕ чекбоксы внутри reasons__items
           reasons.find('input[type="checkbox"]').prop('checked', false);
-
       } else if (redChecked) {
-          // Убираем disabled
           reasons.removeClass('disabled');
       }
   });
 }
+
 toggleReasons();
